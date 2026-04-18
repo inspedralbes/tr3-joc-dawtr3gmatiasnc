f6241da3a5161516b4174ebd136a16f08aa9f8c0
maatiasnc <a24matnegcar@inspedralbes.cat>
2026-04-18 08:46:49 +0200

    Prueba

--- Survivor/server/src/Infrastructure/http/userRoutes.js
+++ Survivor/server/src/Infrastructure/http/userRoutes.js
@@ -3,10 +3,26 @@ const router = express.Router();
 
 module.exports = (userController) => {
 
-   
+
     router.post('/register', userController.register.bind(userController));
 
     router.post('/login', userController.login.bind(userController));
 
     return router;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
